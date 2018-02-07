f327c1602ddf0892be69ae329fc194e97d166d71
Leandro Kimura <leandro.kimura@mirumagency.com>
2018-02-07 17:04:15 -0200

    Module 21.

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,12 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass ({
+  apiKey: "AIzaSyB1JNasZUjizlvgfifmwTAqlY0OrLHpwNU",
+   authDomain: "catch-of-the-day-50e4b.firebaseapp.com",
+   databaseURL: "https://catch-of-the-day-50e4b.firebaseio.com",
+   projectId: "catch-of-the-day-50e4b",
+   storageBucket: "catch-of-the-day-50e4b.appspot.com",
+   messagingSenderId: "1002459147000"
+});
+
+export default base;
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { formatPrice } from '../helpers'
 
 class Order extends React.Component {
+
+  constructor() {
+    super();
+    this.renderOrder = this.renderOrder.bind(this);
+  }
+
+  renderOrder(key) {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const removeButton = <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
+
+    if(!fish || fish.status === 'unavailable'){
+      return <li key={key}> Sorry, {fish ? fish.name : 'fish'} is no longer available. {removeButton}</li>
+    }
+
+    return (
+      <li key={key}>
+        <span>{count}lbs {fish.name} {removeButton}</span>
+        <span className="price">{formatPrice(count * fish.price)}</span>
+      </li>
+    )
+  }
+
   render() {
+    const ordersId = Object.keys(this.props.order)
+    const total = ordersId.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === 'available';
+      if (isAvailable) {
+        return prevTotal + (count * fish.price || 0 )
+      }
+      return prevTotal;
+    }, 0);
+
     return(
-      <p>Order</p>
+      <div>
+        <h2>Your order</h2>
+        <ul className="order">
+          {ordersId.map(this.renderOrder)}
+          <li className="total">
+            <strong>Total:</strong>
+            {formatPrice(total)}
+          </li>
+        </ul>
+      </div>
+
     )
   }
 }
